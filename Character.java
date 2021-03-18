02b5cf4033759eb8e9950fd7b569b0634d4aa72e
Abdelali Mahboub <45123407+abdelalimah@users.noreply.github.com>
2021-03-18 14:35:24 +0100

    Refactor Pingouin to Character class for more genericity

---
+++ Character.java
@@ -0,0 +1,149 @@
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.Vector;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+import java.awt.*;
+
+/**
+ * Pingouin
+ */
+public class Character extends JPanel implements Runnable {
+    
+    private Thread runner;
+    private int x,y;
+    private String name;
+    private String path = "characters/";
+    private int height,width;
+    private Hashtable<String,Integer> sources;
+    private String ext;
+    private int rythme;
+    private int whichImage = 0;
+    private Image[] images;
+    private XMLNode root;
+    private int step = 3;
+
+    public Character(String characterName, int x, int y){
+
+        this.name = characterName;
+        root = new XMLNode(characterName);
+        this.height = root.extractIntAttribute("height");
+        this.width = root.extractIntAttribute("width");
+        rythme = root.extractIntAttribute("rythme");
+        this.x = x;
+        this.y = y;
+        setLocation(x, y);
+        setSize(height,width);
+        this.runner = new Thread(this);
+        loadSources();
+        loadImagesFromSources();
+    }
+
+    void loadSources(){
+        sources = new Hashtable();
+        XMLNode sourcesNode = root.extractChild("sources");
+        this.ext = sourcesNode.extractAttribute("ext");
+        XMLNode[] sourceChildNodes = sourcesNode.extractChildren();
+        for (int i = 0; i < sourceChildNodes.length; i++) {   
+            sources.put(sourceChildNodes[i].textValue(), i);
+        }
+    }
+
+    void loadImagesFromSources(){
+        images = new Image[sources.size()];
+        for (String value : sources.keySet()) {
+            String completePath = path+name+"/icons/"+value+ext;
+            System.out.println(completePath);
+            images[sources.get(value)] = new ImageIcon(completePath).getImage();
+        }
+        /**
+         * sources tabel : {
+         * "left1": 1
+         * "left2": 2
+         * }
+         */
+    }
+
+    void start(){
+        this.runner.start();
+    }
+
+    void characterAction(String actionName){
+        XMLNode actions = root.extractChild("actions");
+        
+        String iconsComposition[] = null;
+
+        for (XMLNode action : actions.extractChildren()) {
+            if(actionName.equals(action.textValue())){
+                iconsComposition = action.extractAttribute("iconsComposition").split(",");
+            }
+        }
+
+        for (int i = 0; i < rythme; i++) {
+            if(iconsComposition != null){
+                for (int j = 0; j < iconsComposition.length ; j++) {
+                    int getCorrespondantIconIndexInSources = sources.get(iconsComposition[j]);
+                    whichImage = getCorrespondantIconIndexInSources;
+                    if("goRight".equals(actionName)){
+                        x+=step;
+                    }else {
+                        x-=step;
+                    }
+                    setLocation(x, y);
+                    repaint();
+                    pause(100);
+                }
+            }
+        }
+    }
+
+    void goRight(){
+        characterAction("goRight");
+    }
+
+    void goLeft(){
+        characterAction("goLeft");
+    }
+
+    void stop(){
+        whichImage = 0;
+        repaint();
+        pause(2000);
+    }
+
+    void pause(int time){
+        try{
+            TimeUnit.MILLISECONDS.sleep(time);
+        }catch(InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(images[whichImage], 0,0, null);
+    }
+    
+    @Override
+    public void run() {
+        while(true){
+            int decision = (int)(Math.random() * 130);
+            if(decision < 50){
+                goRight();
+            }
+            else if(decision < 90){
+                goLeft();
+            }else {
+                stop();
+            }
+        }
+    }
+
+}
---
+++ XMLNode.java
@@ -0,0 +1,100 @@
+import org.w3c.dom.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.Vector;
+
+/*
+* The XMLNode class unlike DocumentParser doesn't care about operations to be performed on an xml document
+* it only cares about the different operations that can be performed on xml **nodes**
+* */
+public class XMLNode {
+    private Node node;
+
+    public XMLNode(Node source){
+        node = source;
+    }
+
+    public XMLNode(String source){
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
+        try{
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse("characters/"+source+"/index.xml");
+            this.node = document.getDocumentElement();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    public XMLNode[] extractChildren(){
+        NodeList nodeList = node.getChildNodes();
+        Vector<XMLNode> v = new Vector<>();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node item = nodeList.item(i);
+            if(isElementNode(item)) v.add(new XMLNode(item));
+        }
+        XMLNode[] t = new XMLNode[v.size()];
+        return v.toArray(t);
+    }
+
+    public XMLNode extractChild(String name){
+        NodeList nodeList = node.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node item = nodeList.item(i);
+            if(item.getNodeName().equals(name)){
+                return new XMLNode(item);
+            }
+        }
+        return null;
+    }
+
+    public String extractAttribute(String name){
+        NamedNodeMap attributes = node.getAttributes();
+        Node attr = attributes.getNamedItem(name);
+        if(attr != null){
+            return attr.getNodeValue();
+        }
+        return null;
+    }
+
+    public int extractIntAttribute(String name){
+        String value = extractAttribute(name);
+        try{
+            return Integer.parseInt(value);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
+
+    public void appendChild(Element childNode){
+        node.appendChild(childNode);
+    }
+
+    public String textValue(){
+        if(node.getNodeType() == Node.ELEMENT_NODE){
+            return node.getFirstChild().getNodeValue();
+        }
+
+        return node.getNodeValue();
+    }
+
+    public boolean isElementNode(Node node){
+        return node.getNodeType() == Node.ELEMENT_NODE;
+    }
+
+    public double doubleValue(){
+        String value = textValue();
+        try{
+            return Double.parseDouble(value);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
+
+}
